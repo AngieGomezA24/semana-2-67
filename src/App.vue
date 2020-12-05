08cea16d845d7a1b9c09214f37dc1bd202a9db28
vamejiari <vameri98@gmail.com>
2020-12-05 17:47:47 -0500

    Agrego foto de Jhon Nestor y la descripcion

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ import TeamCard from "./components/TeamCard.vue";
 import imagenAngie from "../src/assets/angie_lizeth_gomez_acevedo.jpeg"
 import imagenMaria from "../src/assets/maria_alejandra_garcia_ortega.jpg"
 import imagenValentina from "../src/assets/valentina_mejia_rios.jpeg"
+import imagenJhon from "../src/assets/jhon_n_beltran.jpeg"
 
 export default {
   name: "App",
@@ -30,30 +31,30 @@ export default {
         {
           codigo: 1,
           nombre: "Angie Lizeth Gómez Acevedo",
-          descripcion: "Descripcion",
+          descripcion: "Tiene 21 años, le gusta realizar caminatas y tomar fotografias",
           rol: "Arquitecta de software",
           imagen: imagenAngie,
         },
         {
           codigo: 2,
           nombre: "Valentina Mejia Rios",
-          descripcion: "Descripcion",
+          descripcion: "Tiene 22 años, le gusta pintar y leer",
           rol: "Desarrolladora Backend",
           imagen: imagenValentina,
         },
         {
           codigo: 3,
           nombre: "Maria Alejandra García Ortega",
-          descripcion: "Descripcion",
+          descripcion: "Tiene 28 años, le gusta hacer Yoga y Meditar",
           rol: "Desarrolladora Frontend",
           imagen: imagenMaria,
         },
         {
           codigo: 4,
           nombre: "Jhon Nelson Beltrán Pinzón",
-          descripcion: "Descripcion",
-          rol: "desarrollador",
-          imagen: "../src/assets/angie_lizeth_gomez_acevedo.jpeg",
+          descripcion: "Tiene 40 años, le gusta aprender, actualizar sus saberes y poder transmirlos",
+          rol: "Tester",
+          imagen: imagenJhon,
         },
       ],
     };
